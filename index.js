47c6d5f96d6a2290886a95dd7f7353029681ca7f
Gloomy <gloomyneter@gmail.com>
2019-05-22 15:57:01 +0800

    通过摄像头捕捉视频并且展示

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')()
+const http = require('http').createServer(express)
+
+http.listen(8080, () => {
+    console.log('http://127.0.0.1:8080');
+})
+
+express.get('/', (req, resp) => {
+    resp.sendFile(__dirname + '/01.通过摄像头捕捉视频并且展示.html')
+})
+
+express.get('/demo', (req, resp) => {
+    resp.sendFile(__dirname + '/demo.html')
+})
+
+express.get('/err', (req, resp) => {
+    console.log(req.query)
+    resp.send(req.query)
+})
